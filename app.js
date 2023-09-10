ef3d2488601fb45ec9ec8dd97d356730475605df
Josephine mwende <josymwesh45@gmail.com>
2023-09-11 00:36:57 +0300

    create and host an endpoint

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/api", (req, res) => {
+    const slack_name = req.query.slack_name;
+    const track = req.query.track;
+
+    //current day of the week
+    const daysOfTheWeek = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const current_day = daysOfTheWeek[new Date().getDay()];
+    
+    //curret UTC time 
+    const now = new Date();
+    const utc_time = now.toISOString();
+
+    //The github repo and file URL
+    const github_repo_url = "https://github.com/Josephinemwende/Backend-Task1.git"
+    const github_file_url = "";
+
+    // response JSON
+    const response = {
+    "slack_name" : "Josephine_Mwenswa",
+    "current_day" : current_day,
+    "utc_time" : utc_time,
+    "track" : track,
+    "github_file_url" : github_repo_url,
+    "github_repo_url" : github_file_url,
+    "status_code" : 200,
+};
+
+res.json(response);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
